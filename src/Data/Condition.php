4bf481bfb822154ab7fdc95c00d62f45ac42c7fe
Matus Stafura <kobe21@gmail.com>
2022-09-30 17:18:21 +0200

    feat: add condition

---
+++ src/Data/Condition.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Matusstafura\FakerCommerce\Data;
+
+final class Condition
+{
+    public static array $condition = [
+        "New",
+        "New other (see details)",
+        "New with defects",
+        "Certified - Refurbished",
+        "Excellent - Refurbished",
+        "Very Good - Refurbished",
+        "Good - Refurbished",
+        "Seller refurbished",
+        "Like New",
+        "Used",
+        "Very Good",
+        "Good",
+        "Acceptable",
+        "For parts or not working",
+    ];
+}
--- src/Faker/Faker.php
+++ src/Faker/Faker.php
@@ -2,13 +2,25 @@
 
 namespace Matusstafura\FakerCommerce\Faker;
 
+use Matusstafura\FakerCommerce\Data\Condition;
 use Matusstafura\FakerCommerce\Data\Stock;
 
 class Faker
 {
+    /**
+     * @return string
+     */
     public function stock(): string
     {
         return Helper::randomize(Stock::$stock);
+    }
+
+    /**
+     * @return string
+     */
+    public function condition(): string
+    {
+        return Helper::randomize(Condition::$condition);
     }
 
 }
